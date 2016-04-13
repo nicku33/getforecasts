8ea3e0ca90f9fd8192897ef1bcb80384d3ca7d26
Nicholas Ursa <nick.ursa@nytimes.com>
2016-04-13 19:41:56 -0400

    Adding weather.gov scraper

--- get_weather_gov.py
+++ get_weather_gov.py
@@ -3,7 +3,6 @@ import requests
 import re
 from bs4 import BeautifulSoup as bs
 url = 'http://forecast.weather.gov/MapClick.php?lat=40.71326027124559&lon=-74.00710399075575#.Vw7PjjZh2cw'
-#response = HtmlResponse(url, body=
 r = requests.get(url)
 if(r.status_code != 200):
     sys.exit(1)
